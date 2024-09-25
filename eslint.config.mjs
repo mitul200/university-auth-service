236412cb7dff6062f67c7fe3f957fe1f73a2f206
mitul200 <mitul5267@gmail.com>
2024-09-25 20:18:02 +0600

    setup eslint pretteir husky lint staged

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-config-prettier'
+
+export default [
+  {
+    ignores: ['.node_modules/*'],
+  },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  {
+    rules: {
+      eqeqeq: 'off',
+      'no-unused-vars': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-app.use(cors());
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfull!");
-});
+app.get('/', (req: Request, res: any) => {
+  res.send('Working successfull!')
+})
 
-export default app;
+export default app
